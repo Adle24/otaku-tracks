dd3d4dc2f3e41d5bca236d1f83f5b592b83e1d1a
Adle24 <askaradilet41@gmail.com>
2023-06-17 14:51:31 +0600

    Added song upload feature

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBaoBa8ZfWyPg7RHwJ78faKeYzgp6Jcp3U',
@@ -14,7 +15,9 @@ firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage()
 
 const usersCollection = db.collection('users')
+const songsCollection = db.collection('songs')
 
-export { auth, db, usersCollection }
+export { auth, db, usersCollection, storage, songsCollection }
